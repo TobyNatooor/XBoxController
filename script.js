c75359a2b75de23babb076d6ee2a92782407956d
tobiasnf <tobiasnf@gmail.com>
2020-08-22 11:21:16 +0200

    .

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 
 
-window.addEventListener("gamepadconnected", e => {
+// window.addEventListener("gamepadconnected", e => {
 
-    let main = document.getElementById('main')
+//     let main = document.getElementById('main')
 
+//     setInterval(() => {
+//         main.innerHTML = ''
+
+//         e.gamepad.axes.forEach(axe => {
+//             main.innerHTML += '<div>' + axe.toFixed(3) + '</div>'
+//         })
+
+//         e.gamepad.buttons.forEach(button => {
+//             main.innerHTML += '<div>' + button.pressed + '</div>'
+//         })
+//     }, 1000)
+// });
+
+this.addEventListener("gamepadconnected", e => {
     setInterval(() => {
-        main.innerHTML = ''
+        // e.gamepad.axes.forEach(axe => {
+        // })
 
-        e.gamepad.axes.forEach(axe => {
-            main.innerHTML += '<div>' + axe.toFixed(3) + '</div>'
-        })
-
-        e.gamepad.buttons.forEach(button => {
-            main.innerHTML += '<div>' + button.pressed + '</div>'
-        })
+        // e.gamepad.buttons.forEach(button => {
+        // })
+        console.clear()
+        console.log('5')
     }, 1000)
 });
